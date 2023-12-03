e833486cee6aef16f16dd3b6297f2458b2e152ea
jusmatol <jusmatol@student.jyu.fi>
2023-12-03 20:45:55 +0200

    Harkkatyö palautusvalmis

---
+++ Demo5.R
@@ -0,0 +1,168 @@
+################################################################################
+
+#Excercise 1
+
+data <- read.csv("C:\\kurssit\\TILA141\\phone_talking.csv")
+n <- nrow(data)
+k <- sum(data$talking)
+
+thetahat <- k/n
+
+#Normaalijakauma-approksimaatio
+thetaci <- c(thetahat + qnorm(0.025,0,1)/sqrt(n), thetahat + qnorm(0.975,0,1)/sqrt(n))
+thetaci
+
+
+
+#Parametriton bootstrap
+nsim <- 1000
+thetahatsim <- rep(NA,nsim)
+
+for(i in 1:nsim)
+{  
+  datasim <- sample(data$talking, replace = TRUE)
+  
+  thetahatsim[i] <- sum(datasim)/length(datasim)
+}
+
+thetaci <- c( quantile(thetahatsim,0.025), quantile(thetahatsim,0.975) )
+
+thetaci
+
+################################################################################
+
+#Excercise 2
+
+library(mnormt)
+
+mu <- c(2,2,1,1)
+
+cov <- cbind(c(2,1.6,0.4,0.4),c(1.6,2,0.4,0.4),c(0.4,0.4,1,0.8),c(0.4,0.4,0.8,1))
+
+x <- rmt(n = 10000, mean = mu, S = cov, df = 8)
+y <- rep(NA, 10000)
+
+for (i in 1:10000) 
+{
+  y[i] <- ((x[i,][1] + x[i,][2])^2) / (1 + abs(x[i,][3]) + abs(x[i,][4]))
+}
+
+nsim <- 1000
+means <- rep(NA, nsim)
+
+#a
+
+for (i in 1:nsim)
+{
+  sy <- sample(y, 10000, replace=TRUE) #Tehdään uusio-otos
+
+  means[i] <- mean(sy) #Lasketaan odotusarvo otoksesta
+}
+
+sd(means)
+
+#b
+
+#x edelleen otos t-jakautuneista satunnaisvektoreista.
+#Oletetaan, että x on normaalijakautunut.
+
+#Määritetään ensin estimoitu odotuarvovektori suurimman uskottavuuden 
+#menetelmällä
+muhat <- c(mean(x[,1]), mean(x[,2]), mean(x[,3]), mean(x[,4]))
+
+#Määritetään keskivirheet suurimman uskottavuuden menetelmällä.
+sigmahat2 <- cov(x)
+
+library(MASS)
+
+#simuloidaan tästä normaalijakaumasta uusia otoksia ja lasketaan niissä
+#Halutun satunnaismuuttujan odotusarvo.
+for (i in 1:nsim)
+{
+   sx <- mvrnorm(10000, mu = muhat, Sigma=sqrt(sigmahat2))
+   for (j in 1:10000) 
+   {
+     y[j] <- ((sx[j,][1] + sx[j,][2])^2) / (1 + abs(sx[j,][3]) + abs(sx[j,][4]))
+   }
+   means[i] <- mean(y)
+}
+
+sd(means)
+
+################################################################################
+
+#Excercise 3
+
+library(boot)
+
+x <- rnorm(20, 0, 1)
+nsim <- 1000
+
+meanfun <- function(d,ind)
+{
+  return(mean(d[ind]))  
+} 
+
+meanboot <- boot(data=x, statistic=meanfun, R=nsim)
+meanci <- boot.ci(meanboot, type = c("norm", "basic", "perc", "bca"))
+
+meanci
+
+################################################################################
+
+#Excercise 4
+
+Hdist <- function(n, theta, nsim)
+{
+  Hs <- rep(NA,nsim)
+  
+  for (j in 1:nsim)
+  {
+    guesses <- rgeom(n, theta) #Simuloidaan ihmisten arvauksia
+    sguesses <- sort(guesses) #Järjestetään arvaukset
+    H <- -1
+    nsame <- 1
+    
+    if (length(sguesses) == 1)
+    {
+      H <- sguesses[1]
+      Hs[j] <- H
+      break
+    }
+    
+    #Etstitään voittava arvaus
+    for (i in 1:(length(sguesses)-1))
+    {
+      if (sguesses[i] != sguesses[i+1])
+      {
+        if (nsame == 1) 
+        {
+          H <- sguesses[i]
+          break
+        }
+        nsame <- 1
+      }
+      else nsame = nsame+1
+    }
+    
+    Hs[j] <- H
+  }
+  
+  #Otetaan pois -1 H-arvot eli kun kukaan ei voittanut.
+  Hs <- Hs[! Hs %in% -1]
+  
+  lambdahat <- sum(Hs)/length(Hs)
+  
+  plot(table(Hs)/length(Hs), xlab="Occurences of different H", ylab="Relative frequency")
+  #lines(1:20, dpois(1:20, lambda=lambdahat), col="red") #Jakauman sovittaminen, mikä?
+}
+
+#Testausta
+
+Hdist(100, 0.1, 10000)
+
+################################################################################
+
+#Excercise 5
+
+################################################################################
--- HT/app.R
+++ HT/app.R
@@ -8,25 +8,23 @@ ui <- fluidPage(
     
     sidebarPanel(
       
-      #Radio buttons to choose the model
-      radioButtons(inputId = "model", h3("Choose the distribution"), 
+      #Radio buttons to choose the distribution
+      radioButtons(inputId = "model", h3("Distribution"), 
         choices = list("Poisson" = 1, "Normal" = 2, "Exponent"=3, "Geometric"=4), selected = 1
       ),
       
       #Slider input to choose the max sample size
       sliderInput(
-        inputId = "n", h3("Choose sample size"), value = 100, min=1, max=10000
+        inputId = "n", h3("Maximum sample size"), value = 100, min=1, max=10000
       ),
-      
-      #Slider to choose the value of the model parameter. 
-      #sliderInput(
-      #  inputId = "theta", h3("Choose the value for estimated parameter"), value = 1, min = 1, max=10
-      #),
       
       uiOutput("thetaSlider")
     ),
       
       mainPanel(
+      p("Plot shows how maximum likelihood estimates get closer to the theoretical value of the model parameter (red line)
+        as the sample size rises."),
+        
       plotOutput("plot")
     )
   )
@@ -35,34 +33,46 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+  #Theta-slider's min and max change depending on the chosen distribution.
   output$thetaSlider <- renderUI({
-    minval <- 0
-    maxval <- 0
-    if (input$model == 1 || input$model == 3) 
+    minval <- 0.1
+    maxval <- 10
+    param <- "\u03bb"
+    if (input$model == 1) 
     {
       minval <- 0.1
       maxval <- 10
+      param <- "\u03bb"
     }
-    else if (input$model == 2 ) 
+    else if (input$model == 2) 
     {
       minval <- -10
       maxval <- 10
+      param <- "\u03bc"
     }
-    else if (input$model == 4 ) 
+    else if (input$model == 3) 
+    {
+      minval <- 0.1
+      maxval <- 10
+      param <- "\u03bb"
+    }
+    else if (input$model == 4) 
     {
       minval <- 0.01
       maxval <- 0.99
+      param <- "p"
     }
     
-    sliderInput("theta", "Theta", min=minval, max=maxval, value=minval)
+    sliderInput("theta", param, min=minval, max=maxval, value=minval)
   })
+  
   
   output$plot <- renderPlot({
     
     theta <- input$theta
     
-    #Generate random numbers from chosen distribution with chosen parameter and
-    #evaluate mle's from all different sample sizesfrom 1 to chosen max sample
+    #Generate random numbers from chosen distribution with chosen parameter (mu for normal) and
+    #evaluate mle's from all different sample sizes from 1 to chosen max sample
     #size.
     if (input$model == 1 ) 
     {
@@ -86,21 +96,21 @@ server <- function(input, output) {
       mle <- 1/(1+cumsum(x)/(1:input$n))
     }
     
-    estimates <- data.frame(sampleSize = 1:input$n, MLE = mle)
+    estimates <- data.frame(n = 1:input$n, MLE = mle)
     
-    #plot(1:input$n, mle)
+    
     #Plot the mle's against sample sizes and also the real value of
     #model parameter.
-    ggplot(data=estimates, aes(x=sampleSize,y=MLE))+
+    ggplot(data=estimates, aes(x=n,y=MLE))+
     geom_point(color="blue")+
-    geom_line(aes(y=theta), col="red")
+    geom_line(aes(y=theta), col="red")+
+    xlab("Sample size")+
+    ylab("Maximum likelihood estimate")
+    
+    #plot(1:input$n, mle)
     #abline(a=theta, b=0, col=2)
   
   })
-  
-   
-  
-  
   
 }
 
