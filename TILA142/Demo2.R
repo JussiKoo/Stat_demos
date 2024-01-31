c0870a06513f8c40629be9ae28d7efe3f7112052
jusmatol <jusmatol@student.jyu.fi>
2024-01-31 15:36:46 +0200

    Demo4 jatkoa

--- TILA142/Demo2.R
+++ TILA142/Demo2.R
@@ -24,3 +24,18 @@ plot(mixm, "density", col ="red")
 lines(density(x,bw="SJ"))
 lines(xs,dnorm(xs,mean(xs),sd(xs)), col="blue")
 
+#==================================================
+#T6
+
+set.seed(20190204)
+theta <- 3
+n <- 200
+Z <- 1 + rbinom(n, 1, 0.7)
+x <- (Z==1) * rpois(n,1) + (Z==2) * rpois(n,3)
+rm(Z)
+
+logL <- function(theta){
+  return(sum(log(0.3*dpois(x, 1) + 0.7*dpois(x, theta))))
+}
+
+optimize(f=logL, interval=c(0,4), maximum=TRUE)
--- TILA142/Demo4.R
+++ TILA142/Demo4.R
@@ -16,5 +16,65 @@ tn1 <- 1 - 0.95^m
 
 tn2 <- 1 - (1-0.05/m)^m
 
-plot(1:100, tn1, "l")
-plot(1:100, tn2, "l", col="red")
+plot(1:100, tn1)
+points(1:100, tn2, col="red")
+
+#T2
+
+#Testisuure Z_k
+#Nollahypoteesin vallitessa noudattaa N(0,1)
+#Vaihtoehtoisen vallitessa noudattaa N(3,1)
+
+#testattavien hypoteesien määrä m=100000
+#0.1% tilanteissa vaihtoehtoinen hypoteesi on tosi.
+
+set.seed(2019)
+
+m <- 100000
+alpha <- 0.05
+
+z1 <- rnorm(99900,0,1)
+z2 <- rnorm(100,3,1)
+
+z <- c(z1, z2)
+
+z <- sample(z)
+reject <- numeric(m)
+
+p.values <- numeric(m)
+
+#a
+
+for (i in 1:m){
+  p.values[i] <- 1-pnorm(z[i], mean = 0, sd = 1)
+  
+  reject[i] <- p.values[i]*m < alpha
+}
+
+sum(reject)/m
+
+#b
+
+p.values.sorted <- sort(p.values)
+
+i <- 1
+
+while (p.values.sorted[i] <= (i/m)*0.2){
+  i <- i+1
+}
+
+i
+
+#================
+
+priori <- 0.1
+
+ptautipos <- (0.85*priori)/(0.85*priori + 0.04*(1-priori))
+
+ptautineg <- (0.15*priori)/(0.15*priori + 0.96*(1-priori))
+
+100*ptautipos+900*ptautineg
+
+
+
+
