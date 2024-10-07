d9bd0a17997b556ad4d602a6d484f85ad489247f
Jussi Kauppinen <jussikauppinen@hotmail.fi>
2024-10-07 15:52:49 +0300

    demo5 ja ht

--- Bayes1/Demo5.R
+++ Bayes1/Demo5.R
@@ -17,3 +17,38 @@ predict(fitsade, newdata = newdata)
 #Ennusteen odotusarvon posteriorikeskiarvo 95% posterioriväli
 fitted(fitsade, newdata = newdata)
 
+#T4
+
+#alpha vastaa tilannetta jossa demopisteet on 0. Tenttipisteet tuskin tulevat
+#olemaan tällöin kovin suuret. Voisi olla esim N(1,1).
+
+#beta kuvaa yhden demopistelisäyksen vaikutusta tenttipistemäärän odotusarvoon.
+#Vaikutuksen voisi kuvitella olevan vähintään positiivinen. Voisi olla
+#esim. N(4,1).
+
+#Simuloidaan näytteitä prioriennustejakaumasta
+
+alphasim <- rnorm(1000, mean=1, sd=1)
+betasim <- rnorm(1000, mean=4, sd=1)
+musim <- alphasim + 3*betasim
+ysim <- rnorm(1000, mean=musim, sd=1)
+plot(density(ysim))
+
+#T5
+
+d <- data.frame(
+  demopisteet = c(
+    6, 5, 6, 5.5, 5.5, 6, 6, 6, 0, 6, 3,
+    5, 4, 6, 4, 5, 5.5, 5.5, 2.5, 4, 0, 6, 0
+  ),
+  tentti = c(
+    15, 20, 16, 15, 14, 21, 24, 17, 7, 17, 13,
+    19, 14, 19, 11, 8, 11, 14, 12, 6, 5, 20, 4
+  )
+)
+
+#Luento 7.10
+
+#Mallinnusjuttuja
+
+
