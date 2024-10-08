015ca716a723858c9df5e7b2d9791cfc99c65ea9
JussiKoo <jussikauppinen@hotmail.fi>
2024-10-08 12:33:12 +0300

    Demo 5 valmis

--- Bayes1/Demo5.R
+++ Bayes1/Demo5.R
@@ -3,7 +3,7 @@ library(brms)
 #T3
 
 #ennusteen odotusarvon posteriorikeskiarvo, ennusteen 95
-#% ennusteväli ja ennusteen odotusarvon 95 % posterioriväli
+#% ennustevC$li ja ennusteen odotusarvon 95 % posteriorivC$li
 
 rain <- read.table("http://users.jyu.fi/~santikka/bayes1/data/rain.txt")
 
@@ -11,30 +11,32 @@ fitsade <- brm(joulukuu ~ marraskuu, data=rain)
 
 newdata = data.frame(marraskuu=c(50))
 
-#Ennusteen posteriorikeskiarvo ja sen 95% ennusteväli
+#Ennusteen posteriorikeskiarvo ja sen 95% ennustevC$li
 predict(fitsade, newdata = newdata)
 
-#Ennusteen odotusarvon posteriorikeskiarvo 95% posterioriväli
+#Ennusteen odotusarvon posteriorikeskiarvo 95% posteriorivC$li
 fitted(fitsade, newdata = newdata)
 
 #T4
 
 #alpha vastaa tilannetta jossa demopisteet on 0. Tenttipisteet tuskin tulevat
-#olemaan tällöin kovin suuret. Voisi olla esim N(1,1).
+#olemaan tällöin kovin suuret. Voisi olla esim N(0,1).
 
 #beta kuvaa yhden demopistelisäyksen vaikutusta tenttipistemäärän odotusarvoon.
 #Vaikutuksen voisi kuvitella olevan vähintään positiivinen. Voisi olla
 #esim. N(4,1).
 
-#Simuloidaan näytteitä prioriennustejakaumasta
+#Simuloidaan nC$ytteitC$ prioriennustejakaumasta
 
-alphasim <- rnorm(1000, mean=1, sd=1)
+alphasim <- rnorm(1000, mean=0, sd=1)
 betasim <- rnorm(1000, mean=4, sd=1)
 musim <- alphasim + 3*betasim
-ysim <- rnorm(1000, mean=musim, sd=1)
+sigmasim <- rgamma(1000,1,1)
+ysim <- rnorm(1000, mean=musim, sd=sigmasim)
 plot(density(ysim))
-
 #T5
+
+set.seed(1)
 
 d <- data.frame(
   demopisteet = c(
@@ -47,8 +49,33 @@ d <- data.frame(
   )
 )
 
-#Luento 7.10
+priors <- c(
+  prior(normal(0,1), class="b", coef="Intercept"),
+  prior(normal(4,1), class="b", coef="demopisteet"),
+  prior(gamma(1,1), class="sigma")
+)
 
-#Mallinnusjuttuja
+fit <- brm(tentti ~ 0 + Intercept + demopisteet, prior = priors, data=d)
+fit
 
+#alpha posteriorikeskiarvo 0.85 ja 95% posterioriväli [-0.87, 2.53]
+#beta posteriorikeskiarvo 2.84 ja 95% posterioriväli [2.39, 3.31]
+#sigma posteriorikeskiarvo 3.82 ja 95% posterioriväli [2.89, 5.09]
+
+#ehdolliset post. mediaanit
+conditional_effects(fit)
+
+#Havaintotason ennusteita
+predict(fit)
+
+#T6
+
+fittrunc <- brm(tentti | trunc(lb=0, ub=24) ~ 0 + Intercept + demopisteet, prior = priors, data=d)
+fittrunc
+
+#ehdolliset posteriorimediaanit
+conditional_effects(fittrunc)
+
+#havaintotason ennusteita
+predict(fittrunc)
 
