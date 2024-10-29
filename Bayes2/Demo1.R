90619c8321db3267d79cd8467a66829ead404cc8
Jussi Kauppinen <jussikauppinen@hotmail.fi>
2024-10-29 11:48:05 +0200

    demo1 aloitettu

---
+++ Bayes2/Demo1.R
@@ -0,0 +1,34 @@
+#Bayes 2 - demo 1
+
+#T4
+
+#Annetaan input otos jakaumasta
+
+#Järjestetään otos
+
+#Lasketaan min ja max
+
+#Kuljetetaan alarajaa minimistä lähtien ja lasketaan mihin asti tulisi katsoa
+#jotta mukaan on laskettu 95% otoksesta. Talletetaan välin pituus.
+
+#Päivitetään välin pituus jos löytyy pienempi.
+
+
+
+
+
+
+
+#T5
+
+
+
+
+
+
+
+
+
+
+
+#T6
