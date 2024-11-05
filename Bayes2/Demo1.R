80c5857448b5fd609aa6684fbb46af7933e9153c
Jussi Kauppinen <jussikauppinen@hotmail.fi>
2024-11-05 11:41:23 +0200

    demo2 pohja

--- Bayes2/Demo1.R
+++ Bayes2/Demo1.R
@@ -2,17 +2,6 @@
 
 #T4
 
-#Annetaan input otos jakaumasta
-
-#Järjestetään otos
-
-#Lasketaan min ja max
-
-#Kuljetetaan alarajaa minimistä lähtien ja lasketaan mihin asti tulisi katsoa
-#jotta mukaan on laskettu 95% otoksesta. Talletetaan välin pituus.
-
-#Päivitetään välin pituus jos löytyy pienempi.
-
 lyhinvali <- function(otos) {
   jarjestetty_otos <- sort(otos)
   
