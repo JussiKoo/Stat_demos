fa0a7321ae0bc0666f1c987dff78cdeedd5bc034
Jussi Kauppinen <jussikauppinen@hotmail.fi>
2024-11-06 19:59:00 +0200

    demo 2 T4 valmis

--- Bayes2/Demo2.R
+++ Bayes2/Demo2.R
@@ -22,6 +22,8 @@
 
 #T3
 
+library(rstan)
+
 y <- c(1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0)
 length(y)
 
@@ -34,10 +36,10 @@ n4 <- 2 #0,1 parit
 
 #(theta2)^6 * (theta1)^3 * (1-theta2)^2 * (1-theta1)^8
 
-stan <- 
+scode <- 
 "data {
   int<lower=0> N;
-  vector[N] y;
+  int y[N];
 }
 parameters {
   real theta1;
@@ -46,13 +48,36 @@ parameters {
 model {
   theta1 ~ uniform(0,1);
   theta2 ~ uniform(0,1);
-  y ~ normal(alpha + beta * x, sigma);
+  for (n in 2:N){
+    if (y[n-1] == 0) y[n] ~ bernoulli(theta1);
+    else y[n] ~ bernoulli(theta2);
+  }
 }"
 
+fit1 <- stan(
+  model_code = scode,
+  data = list(N = length(y), y = y),
+  iter = 2000
+)
 
+params <- extract(fit1)
 
+theta1est <- mean(params$theta1)
+theta2est <- mean(params$theta2)
+Ptheta <- matrix(
+  c(
+    1-theta1est, 1-theta2est,
+    theta1est, theta2est
+  ),
+  2,
+  2
+)
+Ptheta
 
 #T4
+
+url <- "http://users.jyu.fi/~santikka/bayes2/data/lankarulla.dat"
+lankarulla <- read.table(url, header = TRUE)
 
 #T5
 
