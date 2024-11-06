5471b367355818506a66ed7171781ac071d3767d
JussiKoo <jussikauppinen@hotmail.fi>
2024-11-06 15:18:15 +0200

    demo 2 lisäyksiä

--- Bayes2/Demo2.R
+++ Bayes2/Demo2.R
@@ -1,6 +1,30 @@
+#T1
+
+#a) target-"muuttuja" kuvaa log-(posteriori)tiheysfunktiota
+
+#===============================================================================
+
+#b) "~"-notaation jälkeinen lauseke lisätään posterioritiheyteen kertoimena.
+#Eli tämä vastaa sikäli lisäämistä lisäystä "target"-muuttujaan.
+
+#Seuraavat rivit vastaavat johtavat siis samaan päättelyyn
+
+#y ~ normal(mu, sigma);
+
+#target += normal_lpdf(y | mu, sigma);
+
+#===============================================================================
+
+#c)
+
+#Normalisoimattomien jakaumien käyttö tekee simuloinnista laskennallisesti
+#edullisempaa.
+
 #T3
 
 y <- c(1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0)
+
+
 
 #T4
 
