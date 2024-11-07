58f1a3dd7a1ae77cfd5b783bae6367d656d2d355
JussiKoo <jussikauppinen@hotmail.fi>
2024-11-07 13:32:20 +0200

    Demo 4 T4 valmis

--- Bayes2/Demo2.R
+++ Bayes2/Demo2.R
@@ -23,20 +23,13 @@
 #T3
 
 library(rstan)
+library(tidyr)
 
 y <- c(1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0)
-length(y)
-
-n1 <- 6 #1,1 parit
-n2 <- 8 #0,0 parit
-n3 <- 3 #1,0 parit
-n4 <- 2 #0,1 parit
-
-#Uskottavuus: 
 
 #(theta2)^6 * (theta1)^3 * (1-theta2)^2 * (1-theta1)^8
 
-scode <- 
+scode3 <- 
 "data {
   int<lower=0> N;
   int y[N];
@@ -54,16 +47,17 @@ model {
   }
 }"
 
-fit1 <- stan(
-  model_code = scode,
+fit3 <- stan(
+  model_code = scode3,
   data = list(N = length(y), y = y),
   iter = 2000
 )
 
-params <- extract(fit1)
+params <- extract(fit3)
 
 theta1est <- mean(params$theta1)
 theta2est <- mean(params$theta2)
+
 Ptheta <- matrix(
   c(
     1-theta1est, 1-theta2est,
@@ -72,12 +66,59 @@ Ptheta <- matrix(
   2,
   2
 )
+
 Ptheta
 
 #T4
 
 url <- "http://users.jyu.fi/~santikka/bayes2/data/lankarulla.dat"
 lankarulla <- read.table(url, header = TRUE)
+lankarulla$pituus <- lankarulla$pituus/100
+eka <- lankarulla[1,2]
+lankarulla[1,2] <- NA
+lankarulla <- drop_na(lankarulla)
+
+lankarulla
+
+
+#Miten toimitaan? Käytetäänkö sovittamiseen kaikkia muita rivejä vai
+#käsitelläänkö tuo yksi havainto ikään kuin sensuroituna?
+
+scode4 <- 
+  "data {
+  int<lower=0> N;
+  vector[N] x;
+  int<lower=0> y[N];
+  real x_tilde;
+}
+parameters {
+  real alpha;
+  real beta;
+}
+model {
+  alpha ~ normal(0,100);
+  beta ~ normal(0,100);
+  y ~ poisson_log(alpha+beta*x);
+}
+generated quantities {
+  int<lower=0> y_tilde = poisson_log_rng(alpha + beta * x_tilde);
+}"
+
+fit4 <- stan(
+  model_code = scode4,
+  data = list(N = nrow(lankarulla), y = lankarulla$virheet, x = lankarulla$pituus, x_tilde = 5.51),
+  iter=2000
+  )
+
+fit4
+
+params4 <- rstan::extract(fit4)
+
+params4$y_tilde
+
+#Posterioriennustejakauma kuvaa tässä tapauksessa "uuteen" havaintoon liittyvää
+#epävarmuutta. Epävarmuus liittyy tässä tapauksessa parametreihin, että havaintoihin.
+
 
 #T5
 
