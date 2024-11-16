07ab0581a6ee7de7c997ea75bf0bd5e177335ab4
JussiKoo <jussikauppinen@hotmail.fi>
2024-11-16 14:53:31 +0200

    Demo 3 valmis

--- Bayes2/Demo3.R
+++ Bayes2/Demo3.R
@@ -29,3 +29,72 @@ fit5 <- stan(
 s <- summary(fit5, pars=c("phi", "theta[4]", "theta[12]"), probs=c(0.025, 0.975))
 
 s$summary
+
+pairs(fit5, pars=c("theta[4]", "theta[12]"))
+plot(fit5, plotfun="hist", pars="phi")
+
+#T6
+
+#1 Aseta alkuarvo theta(t) = theta(0)
+
+#2 Generoi ehdotus theta* ehdotusjakaumasta Tas(theta(t)-a, theta(t)+a)
+
+#3 Laske hyväksymistodennäköisyys normeerattomien posteriorijakaumien avulla
+
+# alpha = min(p(theta*)p(y|theta*) / (p(theta(t))p(y|theta(t))))
+
+#4 Aseta theta(t+1) = theta* tn alpha, muutoin theta(t)
+
+metropolis <- function(n, k, a){
+  theta <- rep(NA, n)
+  
+  theta[1] <- rt(1,k)
+  
+  for (i in 1:(n-1)){
+    theta_c <- runif(1,theta[i]-a, theta[i]+a)
+    alpha <- min(1, ((1+(theta_c^2)/k)^(-1/2 * (k+1)))/((1+(theta[i]^2)/k)^(-1/2 * (k+1))))
+    theta[i+1] <- ifelse(rbinom(1, 1, alpha) == 1, theta_c, theta[i])
+  }
+  
+  return (theta)
+}
+
+#b
+
+library(coda)
+
+iter=4000
+x <- 1:iter
+
+thetai <- metropolis(iter, 9, 0.3)
+plot(thetai ~ x, type="l")
+hist(thetai, breaks=40)
+
+thetaii <- metropolis(iter, 9, 1.5)
+plot(thetaii ~ x, type="l")
+hist(thetaii, breaks=40)
+
+thetaiii <- metropolis(iter, 9, 2)
+plot(thetaiii ~ x, type="l")
+hist(thetaiii, breaks=40)
+
+thetaiv <- metropolis(iter, 9, 4)
+plot(thetaiv ~ x, type="l")
+hist(thetaiv, breaks=40)
+
+autocorr.plot(mcmc(thetai), 50)
+autocorr.plot(mcmc(thetaii), 50)
+autocorr.plot(mcmc(thetaiii), 50)
+autocorr.plot(mcmc(thetaiv), 50)
+
+#iv ketju näyttää kohisevan tasapainotilan ympärillä
+#autokorrelaatio näyttää myös vähäiseltä
+
+#c 
+
+mean(thetaiv)
+
+var(thetaiv)
+9/(9-2)
+
+#Näyttää estimoituvan melko hyvin
