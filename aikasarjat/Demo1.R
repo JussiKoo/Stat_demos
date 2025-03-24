30bc85dc67eab8b7afa360c7f792691b696f2de2
JussiKoo <jussikauppinen@hotmail.fi>
2025-03-24 14:46:40 +0200

    R-tehtävät melkein done

--- aikasarjat/Demo1.R
+++ aikasarjat/Demo1.R
@@ -20,3 +20,49 @@ ts.plot(ue, ue_smooth, gpars=list(xlab="vuosi", ylab="työttömien osuus",
                                   col=c("blue","red")))
 
 #c
+
+ue_r <- ue - ue_smooth
+ts.plot(ue_r, gpars=list(xlab="vuosi", ylab="jäännös"))
+
+acf(ue_r, na.action=na.omit)
+
+#Tätä voisi tulkita niin, että jäännökset eivät todennäköisesti 
+#ole vain valkoista kohinaa. Peräkkäisten jäännösten käyttäytymisessä
+#eri viiveillä on havaittavissa säännönmukaisuutta.
+
+#===============================================================================
+
+#T2
+
+#a
+
+m <- month.abb[cycle(ue)]
+
+model <- lm(ue_r ~ m)
+model
+
+#b
+
+plot(1:length(ue_r), ue_r, type="p", col="red")
+lines(1:length(fitted(model)), fitted(model), type="l")
+
+plot(resid(model))
+
+#Residuaalit jakautuvat melko tasaisesti nollan ympärille. 
+#Alhaisilla indekseillä on toisaalta havaittavissa hieman suurempia residuaaleja
+#kuin muualla.
+
+#c
+
+ue.d <- decompose(ue)
+plot(ue.d)
+
+#Kausittainen vaihtelu näyttää hyvin samalta kuin sovitetun mallin antamat
+#ennusteet
+
+#d
+
+ue.stl <- stl(ue, "per")
+plot(ue.stl)
+
+#Pohdinnat vielä
